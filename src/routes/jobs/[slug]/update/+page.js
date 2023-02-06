803de4cf0c35078afb52436fc4378f0a8e1d3266
Hong <gowipehong@gmail.com>
2023-02-06 14:21:25 +0800

    adding some code

--- src/routes/jobs/[slug]/update/+page.js
+++ src/routes/jobs/[slug]/update/+page.js
@@ -1,8 +1,26 @@
+import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public'
 import { redirect } from '@sveltejs/kit'
-import { validToken } from "../../../utils/auth"
+import { validToken, getUserId } from "../../../../utils/auth.js"
 
-export async function load(){
+export async function load({fetch, params}){
   if(!(await validToken())){
-    throw redirect(301, '/users/new')
+    throw redirect (301, '/users/new')
+  }
+
+  const resp = await fetch(PUBLIC_BACKEND_BASE_URL + `/api/collections/jobs/records/${params.slug}`)
+
+  const res = await resp.json()
+  if(resp.status == 200){
+    if(res.user != getUserId()){
+      throw redirect(301, '/users/new')
+    }
+
+    return {
+      job: res
+    }
+  } else {
+    return {
+      jobs: []
+    }
   }
 }
