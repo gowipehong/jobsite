602039aced912b54eaba5b7195ef84d578360bc6
Hong <gowipehong@gmail.com>
2023-02-10 16:59:32 +0800

    removing non-used code

--- src/routes/jobs/[slug]/+page.js
+++ src/routes/jobs/[slug]/+page.js
@@ -1,12 +1,6 @@
 import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public'
-import { isLoggedOn } from '../../../utils/auth.js'
-import { validToken } from '../../../utils/auth.js'
 
 export async function load({ fetch, params }) {
-
-  if (await validToken()) {
-    isLoggedOn.set(true)
-  }
 
   const resp = await fetch(PUBLIC_BACKEND_BASE_URL + `/api/collections/jobs/records/${params.slug}`);
 
--- src/routes/jobs/[slug]/update/+page.js
+++ src/routes/jobs/[slug]/update/+page.js
@@ -1,11 +1,8 @@
 import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public'
 import { redirect } from '@sveltejs/kit'
-import { validToken, getUserId } from "../../../../utils/auth.js"
+import { getUserId } from "../../../../utils/auth.js"
 
 export async function load({ fetch, params }) {
-  if (!(await validToken())) {
-    throw redirect(301, '/users/new')
-  }
 
   const resp = await fetch(PUBLIC_BACKEND_BASE_URL + `/api/collections/jobs/records/${params.slug}`)
 
